d8f8d8548cdb00e54e9971bd1957e0dae692b13d
Rangga Eki Syahrial <syahrialrangga11@gmail.com>
2021-03-04 22:04:34 +0700

    Design Auth Page

--- client/index.js
+++ client/index.js
@@ -1,18 +1,20 @@
 $(document).ready(() => {
-  const dummy = `<h1>Lorem ipsum</h1>`;
-  for (let index = 0; index < 25; index++) {
-    $("#content").append(dummy);
-  }
-
-  $(document).scroll(function () {
-    if ($(document).height() - $(document).scrollTop() < 800) {
-      addTitle(dummy);
-    }
-  });
+  $("#form-register").hide();
 });
 
-function addTitle(content) {
-  for (let index = 0; index < 25; index++) {
-    $("#content").append(content);
-  }
-}
+// const dummy = `<h1>Lorem ipsum</h1>`;
+// for (let index = 0; index < 25; index++) {
+//   $("#content").append(dummy);
+// }
+
+// $(document).scroll(function () {
+//   if ($(document).height() - $(document).scrollTop() < 800) {
+//     addTitle(dummy);
+//   }
+// });
+
+// function addTitle(content) {
+//   for (let index = 0; index < 25; index++) {
+//     $("#content").append(content);
+//   }
+// }
